da233fdafcf5e0d0cb518f58013e4bebe7ebcb98
Lode Lauri <lauri.lode@solteq.com>
2018-09-07 13:56:54 +0300

    comspa-tehtavia

--- CompSta1/testi.py
+++ CompSta1/testi.py
@@ -1,11 +1,17 @@
-%matplotlib inline
+import scipy.special
 import numpy as np
-import numpy.random as npr
-import matplotlib.pyplot as plt
 
-x = np.float64(1)
-while(x != np.inf):
-    if(x+1 == np.inf):
-        break
-    x = x+1
-print(x)
+print(0-1)
+
+test = scipy.special.binom(10,0)*np.power(0.25,0)*np.power((1-0.25),(0-1))
+print(test)
+def binprob(l,u,n,p):
+    x = 0
+    for i in range(u):
+        x = x + scipy.special.binom(n,i)*np.power(p,i)*np.power((1-p),(i-1))
+    return x
+
+f1 = binprob(0,5,10,0.25)
+
+
+print(f1)
