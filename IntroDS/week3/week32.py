3bbf285f618e2c0cc010954c743d4961436fa012
unknown <lauri.lode@gmail.com>
2018-09-22 21:23:35 +0300

    week 3 asd

--- IntroDS/week3/week32.py
+++ IntroDS/week3/week32.py
@@ -11,6 +11,7 @@ from sklearn import metrics
 import seaborn as sns
 from collections import Counter
 import scipy
+import matplotlib.pyplot as plt
 
 npr.seed(3456456)
 
@@ -31,10 +32,10 @@ labels = hasy.symbol_id
 
 pics = np.asarray(pics)
 pics2 = pics[:,:,:,0].reshape(len(pics), 32*32)
-
+labels = np.asarray(labels)
 #print(labels)
 
-pics2_train, pics2_test, labels_train, labels_test = train_test_split(pics2, labels, test_size=0.2)
+pics2_train, pics2_test, labels_train, labels_test = train_test_split(pics2, labels, test_size=0.2, random_state=0)
 print(pics2_train.shape)
 print(labels_train.shape)
 
@@ -49,7 +50,7 @@ def most_common(lst):
     data = Counter(lst)
     return max(lst, key=data.get)
 
-naive = labels_train.values
+naive = labels_train
 
 right = 0
 for i in labels_test:
@@ -65,8 +66,25 @@ print('Accuracy of naive guesses on the test set: ', right/len(labels_test))
 confutse = metrics.confusion_matrix(labels_test, y_pred)
 print(confutse)
 
-m = confutse.shape[0]
+""" m = confutse.shape[0]
 strided = np.lib.stride_tricks.as_strided
 s0,s1 = confutse.strides
 out = strided(confutse.ravel()[1:], shape=(m-1,m), strides=(s0+s1,s1)).reshape(m,-1)
-print(out)
+print(out) """
+
+index = 0
+misclassifiedIndexes = []
+for label, predict in zip(labels_test, y_pred):
+    if label != predict:
+        #print(label, predict)
+        misclassifiedIndexes.append(index)
+    index +=1
+
+print(misclassifiedIndexes)
+
+plt.figure(figsize=(20,4))
+for plotIndex, badIndex in enumerate(misclassifiedIndexes[0:5]):
+    plt.subplot(1, 5, plotIndex + 1)
+    plt.imshow(np.reshape(pics2_test[badIndex], (32,32)), cmap=plt.cm.gray)
+    plt.title('Predicted: {}, Actual: {}'.format(y_pred[badIndex], labels_test[badIndex]), fontsize = 15)
+    plt.show()
