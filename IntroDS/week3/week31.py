6901963e35016f7aa073accdda5cdee736eeae95
Lode Lauri <lauri.lode@solteq.com>
2018-09-25 12:43:32 +0300

    week3 final

--- IntroDS/week3/week31.py
+++ IntroDS/week3/week31.py
@@ -13,15 +13,3 @@ cities = gpd.read_file(filepath+'cities\\cities.shp')
 cities = cities.to_crs(world.crs)
 base = world.plot(color='white', edgecolor='black')
 cities.plot(ax=base, marker='o', color='red', markersize=5)
-
-hasy_full = pd.read_csv(filepath+'HASYv2\\hasy-data-labels.csv')
-hasy = hasy_full.query('70<=symbol_id<=80')
-
-hasy.head(10)
-
-pics = np.array([])
-
-# for pic in hasy.symbol_id:
-#    np.append(pics, imageio.imread(filepath+'HASYv2\\hasy-data\\' +pic))
-
-# print(len(pics))
